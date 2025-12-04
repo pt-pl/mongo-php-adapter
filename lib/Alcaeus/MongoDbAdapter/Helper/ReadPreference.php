07be3fa937b7a4add8ea345489ae3522f5e3249b
Daniel Śmigiela <daniel.smigiela@outlook.com>
2025-12-04 09:31:25 +0100

    ~ poprawki do php81

--- lib/Alcaeus/MongoDbAdapter/Helper/ReadPreference.php
+++ lib/Alcaeus/MongoDbAdapter/Helper/ReadPreference.php
@@ -20,132 +20,132 @@ namespace Alcaeus\MongoDbAdapter\Helper;
  */
 trait ReadPreference
 {
-    /**
-     * @var \MongoDB\Driver\ReadPreference
-     */
-    protected $readPreference;
+	/**
+	 * @var \MongoDB\Driver\ReadPreference
+	 */
+	protected $readPreference;
 
-    /**
-     * @param string $readPreference
-     * @param array $tags
-     * @return bool
-     */
-    abstract public function setReadPreference($readPreference, $tags = null);
+	/**
+	 * @param string $readPreference
+	 * @param array $tags
+	 * @return bool
+	 */
+	abstract public function setReadPreference($readPreference, $tags = null);
 
-    /**
-     * @return array
-     */
-    public function getReadPreference()
-    {
-        if ($this->readPreference === null) {
-            $this->readPreference = new \MongoDB\Driver\ReadPreference(\MongoDB\Driver\ReadPreference::RP_PRIMARY);
-        }
+	/**
+	 * @return array
+	 */
+	public function getReadPreference()
+	{
+		if ($this->readPreference === null) {
+			$this->readPreference = new \MongoDB\Driver\ReadPreference(\MongoDB\Driver\ReadPreference::PRIMARY);
+		}
 
-        $mode = $this->readPreference->getMode();
+		$mode = $this->readPreference->getModeString();
 
-        switch ($mode) {
-            case \MongoDB\Driver\ReadPreference::RP_PRIMARY_PREFERRED:
-                $type = \MongoClient::RP_PRIMARY_PREFERRED;
-                break;
-            case \MongoDB\Driver\ReadPreference::RP_SECONDARY:
-                $type = \MongoClient::RP_SECONDARY;
-                break;
-            case \MongoDB\Driver\ReadPreference::RP_SECONDARY_PREFERRED:
-                $type = \MongoClient::RP_SECONDARY_PREFERRED;
-                break;
-            case \MongoDB\Driver\ReadPreference::RP_NEAREST:
-                $type = \MongoClient::RP_NEAREST;
-                break;
-            default:
-                $type = \MongoClient::RP_PRIMARY;
-        }
+		switch ($mode) {
+			case \MongoDB\Driver\ReadPreference::PRIMARY_PREFERRED:
+				$type = \MongoClient::RP_PRIMARY_PREFERRED;
+				break;
+			case \MongoDB\Driver\ReadPreference::SECONDARY:
+				$type = \MongoClient::RP_SECONDARY;
+				break;
+			case \MongoDB\Driver\ReadPreference::SECONDARY_PREFERRED:
+				$type = \MongoClient::RP_SECONDARY_PREFERRED;
+				break;
+			case \MongoDB\Driver\ReadPreference::NEAREST:
+				$type = \MongoClient::RP_NEAREST;
+				break;
+			default:
+				$type = \MongoClient::RP_PRIMARY;
+		}
 
-        $readPreference = ['type' => $type];
-        if ($this->readPreference->getTagSets() !== null && $this->readPreference->getTagSets() !== []) {
-            $readPreference['tagsets'] = $this->readPreference->getTagSets();
-        }
+		$readPreference = ['type' => $type];
+		if ($this->readPreference->getTagSets() !== null && $this->readPreference->getTagSets() !== []) {
+			$readPreference['tagsets'] = $this->readPreference->getTagSets();
+		}
 
-        return $readPreference;
-    }
+		return $readPreference;
+	}
 
-    /**
-     * @return bool
-     */
-    protected function getSlaveOkayFromReadPreference()
-    {
-        return $this->readPreference->getMode() != \MongoDB\Driver\ReadPreference::RP_PRIMARY;
-    }
+	/**
+	 * @return bool
+	 */
+	protected function getSlaveOkayFromReadPreference()
+	{
+		return $this->readPreference->getModeString() != \MongoDB\Driver\ReadPreference::PRIMARY;
+	}
 
-    /**
-     * @param string $readPreference
-     * @param array $tags
-     * @return bool
-     */
-    protected function setReadPreferenceFromParameters($readPreference, $tags = null)
-    {
-        // @internal Passing an array for $readPreference is necessary to avoid conversion voodoo
-        // It should not be used externally!
-        if (is_array($readPreference)) {
-            return $this->setReadPreferenceFromArray($readPreference);
-        }
+	/**
+	 * @param string $readPreference
+	 * @param array $tags
+	 * @return bool
+	 */
+	protected function setReadPreferenceFromParameters($readPreference, $tags = null)
+	{
+		// @internal Passing an array for $readPreference is necessary to avoid conversion voodoo
+		// It should not be used externally!
+		if (is_array($readPreference)) {
+			return $this->setReadPreferenceFromArray($readPreference);
+		}
 
-        switch ($readPreference) {
-            case \MongoClient::RP_PRIMARY:
-                $mode = \MongoDB\Driver\ReadPreference::RP_PRIMARY;
-                break;
-            case \MongoClient::RP_PRIMARY_PREFERRED:
-                $mode = \MongoDB\Driver\ReadPreference::RP_PRIMARY_PREFERRED;
-                break;
-            case \MongoClient::RP_SECONDARY:
-                $mode = \MongoDB\Driver\ReadPreference::RP_SECONDARY;
-                break;
-            case \MongoClient::RP_SECONDARY_PREFERRED:
-                $mode = \MongoDB\Driver\ReadPreference::RP_SECONDARY_PREFERRED;
-                break;
-            case \MongoClient::RP_NEAREST:
-                $mode = \MongoDB\Driver\ReadPreference::RP_NEAREST;
-                break;
-            default:
-                trigger_error("The value '$readPreference' is not valid as read preference type", E_USER_WARNING);
-                return false;
-        }
+		switch ($readPreference) {
+			case \MongoClient::RP_PRIMARY:
+				$mode = \MongoDB\Driver\ReadPreference::PRIMARY;
+				break;
+			case \MongoClient::RP_PRIMARY_PREFERRED:
+				$mode = \MongoDB\Driver\ReadPreference::PRIMARY_PREFERRED;
+				break;
+			case \MongoClient::RP_SECONDARY:
+				$mode = \MongoDB\Driver\ReadPreference::SECONDARY;
+				break;
+			case \MongoClient::RP_SECONDARY_PREFERRED:
+				$mode = \MongoDB\Driver\ReadPreference::SECONDARY_PREFERRED;
+				break;
+			case \MongoClient::RP_NEAREST:
+				$mode = \MongoDB\Driver\ReadPreference::NEAREST;
+				break;
+			default:
+				trigger_error("The value '$readPreference' is not valid as read preference type", E_USER_WARNING);
+				return false;
+		}
 
-        if ($readPreference == \MongoClient::RP_PRIMARY && !empty($tags)) {
-            trigger_error("You can't use read preference tags with a read preference of PRIMARY", E_USER_WARNING);
-            return false;
-        }
+		if ($readPreference == \MongoClient::RP_PRIMARY && !empty($tags)) {
+			trigger_error("You can't use read preference tags with a read preference of PRIMARY", E_USER_WARNING);
+			return false;
+		}
 
-        $this->readPreference = new \MongoDB\Driver\ReadPreference($mode, $tags);
+		$this->readPreference = new \MongoDB\Driver\ReadPreference($mode, $tags);
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * @param array $readPreferenceArray
-     * @return bool
-     */
-    protected function setReadPreferenceFromArray($readPreferenceArray)
-    {
-        $readPreference = $readPreferenceArray['type'];
-        $tags = isset($readPreferenceArray['tagsets']) ? $readPreferenceArray['tagsets'] : [];
+	/**
+	 * @param array $readPreferenceArray
+	 * @return bool
+	 */
+	protected function setReadPreferenceFromArray($readPreferenceArray)
+	{
+		$readPreference = $readPreferenceArray['type'];
+		$tags = isset($readPreferenceArray['tagsets']) ? $readPreferenceArray['tagsets'] : [];
 
-        return $this->setReadPreferenceFromParameters($readPreference, $tags);
-    }
+		return $this->setReadPreferenceFromParameters($readPreference, $tags);
+	}
 
-    /**
-     * @param bool $ok
-     * @return bool
-     */
-    protected function setReadPreferenceFromSlaveOkay($ok = true)
-    {
-        $result = $this->getSlaveOkayFromReadPreference();
-        $readPreference = new \MongoDB\Driver\ReadPreference(
-            $ok ? \MongoDB\Driver\ReadPreference::RP_SECONDARY_PREFERRED : \MongoDB\Driver\ReadPreference::RP_PRIMARY,
-            $ok ? $this->readPreference->getTagSets() : []
-        );
+	/**
+	 * @param bool $ok
+	 * @return bool
+	 */
+	protected function setReadPreferenceFromSlaveOkay($ok = true)
+	{
+		$result = $this->getSlaveOkayFromReadPreference();
+		$readPreference = new \MongoDB\Driver\ReadPreference(
+			$ok ? \MongoDB\Driver\ReadPreference::SECONDARY_PREFERRED : \MongoDB\Driver\ReadPreference::PRIMARY,
+			$ok ? $this->readPreference->getTagSets() : []
+		);
 
-        $this->readPreference = $readPreference;
+		$this->readPreference = $readPreference;
 
-        return $result;
-    }
+		return $result;
+	}
 }
